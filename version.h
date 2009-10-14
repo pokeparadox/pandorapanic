79d0bb328699adbe77ba96acbc7b65999ea68a97
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2009-10-14 23:25:25 +0000

    Updated: Instructions screen for StateRotDef finished.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "07";
+	static const char DATE[] = "15";
 	static const char MONTH[] = "10";
 	static const char YEAR[] = "2009";
 	static const double UBUNTU_VERSION_STYLE = 9.10;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 2;
-	static const long BUILD = 1398;
-	static const long REVISION = 7603;
+	static const long BUILD = 1461;
+	static const long REVISION = 7940;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3273;
-	#define RC_FILEVERSION 1,2,1398,7603
-	#define RC_FILEVERSION_STRING "1, 2, 1398, 7603\0"
-	static const char FULLVERSION_STRING[] = "1.2.1398.7603";
+	static const long BUILDS_COUNT = 3391;
+	#define RC_FILEVERSION 1,2,1461,7940
+	#define RC_FILEVERSION_STRING "1, 2, 1461, 7940\0"
+	static const char FULLVERSION_STRING[] = "1.2.1461.7940";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 98;
+	static const long BUILD_HISTORY = 161;
 	
 
 }
