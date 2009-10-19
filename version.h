41434e357c74ab88015b75f02fd3b711585b04bb
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2009-10-19 13:30:17 +0000

    Fixed: Some misc undefined variable bugs that valgrind was reporting.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "15";
+	static const char DATE[] = "19";
 	static const char MONTH[] = "10";
 	static const char YEAR[] = "2009";
 	static const double UBUNTU_VERSION_STYLE = 9.10;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 2;
-	static const long BUILD = 1530;
-	static const long REVISION = 8346;
+	static const long BUILD = 1534;
+	static const long REVISION = 8367;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3510;
-	#define RC_FILEVERSION 1,2,1530,8346
-	#define RC_FILEVERSION_STRING "1, 2, 1530, 8346\0"
-	static const char FULLVERSION_STRING[] = "1.2.1530.8346";
+	static const long BUILDS_COUNT = 3516;
+	#define RC_FILEVERSION 1,2,1534,8367
+	#define RC_FILEVERSION_STRING "1, 2, 1534, 8367\0"
+	static const char FULLVERSION_STRING[] = "1.2.1534.8367";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 230;
+	static const long BUILD_HISTORY = 234;
 	
 
 }
