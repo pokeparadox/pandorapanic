4fa6be2b364d828189e578beb704d376079c04c8
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2009-10-26 00:03:08 +0000

    Dificulties added to Rotating Turret game.

--- trunk/version.h
+++ trunk/version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "23";
+	static const char DATE[] = "25";
 	static const char MONTH[] = "10";
 	static const char YEAR[] = "2009";
 	static const double UBUNTU_VERSION_STYLE = 9.10;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 2;
-	static const long BUILD = 1551;
-	static const long REVISION = 8451;
+	static const long BUILD = 1572;
+	static const long REVISION = 8579;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3543;
-	#define RC_FILEVERSION 1,2,1551,8451
-	#define RC_FILEVERSION_STRING "1, 2, 1551, 8451\0"
-	static const char FULLVERSION_STRING[] = "1.2.1551.8451";
+	static const long BUILDS_COUNT = 3585;
+	#define RC_FILEVERSION 1,2,1572,8579
+	#define RC_FILEVERSION_STRING "1, 2, 1572, 8579\0"
+	static const char FULLVERSION_STRING[] = "1.2.1572.8579";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 251;
+	static const long BUILD_HISTORY = 272;
 	
 
 }
