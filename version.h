0e6cbb19db78a2c17754e2f4b6bfe5e4e7dca15a
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2009-11-19 20:52:03 +0000

    Updated: Optimised PNGs now work fine.

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "13";
+	static const char DATE[] = "19";
 	static const char MONTH[] = "11";
 	static const char YEAR[] = "2009";
 	static const double UBUNTU_VERSION_STYLE = 9.11;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 2;
-	static const long BUILD = 1619;
-	static const long REVISION = 8876;
+	static const long BUILD = 1644;
+	static const long REVISION = 9002;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3668;
-	#define RC_FILEVERSION 1,2,1619,8876
-	#define RC_FILEVERSION_STRING "1, 2, 1619, 8876\0"
-	static const char FULLVERSION_STRING[] = "1.2.1619.8876";
+	static const long BUILDS_COUNT = 3718;
+	#define RC_FILEVERSION 1,2,1644,9002
+	#define RC_FILEVERSION_STRING "1, 2, 1644, 9002\0"
+	static const char FULLVERSION_STRING[] = "1.2.1644.9002";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 319;
+	static const long BUILD_HISTORY = 344;
 	
 
 }
