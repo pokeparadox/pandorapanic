fa88fb8d50ed6c279711d9ad6507016ab6b6f95b
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2009-12-02 23:31:27 +0000

    Fixed: segfault when displaying pause screen for Pang. Updated: pause key for SpellingContest. Updated: pause key for DistractedMaths.

--- trunk/version.h
+++ trunk/version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 3;
-	static const long BUILD = 1786;
-	static const long REVISION = 9705;
+	static const long BUILD = 1789;
+	static const long REVISION = 9715;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3984;
-	#define RC_FILEVERSION 1,3,1786,9705
-	#define RC_FILEVERSION_STRING "1, 3, 1786, 9705\0"
-	static const char FULLVERSION_STRING[] = "1.3.1786.9705";
+	static const long BUILDS_COUNT = 3987;
+	#define RC_FILEVERSION 1,3,1789,9715
+	#define RC_FILEVERSION_STRING "1, 3, 1789, 9715\0"
+	static const char FULLVERSION_STRING[] = "1.3.1789.9715";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 86;
+	static const long BUILD_HISTORY = 89;
 	
 
 }
