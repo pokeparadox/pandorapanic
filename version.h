580dbde114623b0d27ebddebd7306af098945c91
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2009-12-16 02:22:27 +0000

    Updated: StateTitle updated code to centre text. to reflect Penjin updates. Updated: StateCredits updated code to centre text. to reflect Penjin updates. Updated: PandoraPanic.cbp layout tidied. (new classes organised.)

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "15";
+	static const char DATE[] = "16";
 	static const char MONTH[] = "12";
 	static const char YEAR[] = "2009";
 	static const double UBUNTU_VERSION_STYLE = 9.12;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 3;
-	static const long BUILD = 1959;
-	static const long REVISION = 10703;
+	static const long BUILD = 1961;
+	static const long REVISION = 10720;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4282;
-	#define RC_FILEVERSION 1,3,1959,10703
-	#define RC_FILEVERSION_STRING "1, 3, 1959, 10703\0"
-	static const char FULLVERSION_STRING[] = "1.3.1959.10703";
+	static const long BUILDS_COUNT = 4284;
+	#define RC_FILEVERSION 1,3,1961,10720
+	#define RC_FILEVERSION_STRING "1, 3, 1961, 10720\0"
+	static const char FULLVERSION_STRING[] = "1.3.1961.10720";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 259;
+	static const long BUILD_HISTORY = 261;
 	
 
 }
