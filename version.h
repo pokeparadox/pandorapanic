bd6e24560356c61e1b4cc2522a32b5774c22f4ba
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-01-01 16:32:28 +0000

    Updated: Images ran through PNG optimisation.

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "30";
-	static const char MONTH[] = "12";
-	static const char YEAR[] = "2009";
-	static const double UBUNTU_VERSION_STYLE = 9.12;
+	static const char DATE[] = "01";
+	static const char MONTH[] = "01";
+	static const char YEAR[] = "2010";
+	static const double UBUNTU_VERSION_STYLE = 10.01;
 	
 	//Software Status
 	static const char STATUS[] = "Release Candidate";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 3;
-	static const long BUILD = 2083;
-	static const long REVISION = 11384;
+	static const long BUILD = 2097;
+	static const long REVISION = 11448;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4507;
-	#define RC_FILEVERSION 1,3,2083,11384
-	#define RC_FILEVERSION_STRING "1, 3, 2083, 11384\0"
-	static const char FULLVERSION_STRING[] = "1.3.2083.11384";
+	static const long BUILDS_COUNT = 4539;
+	#define RC_FILEVERSION 1,3,2097,11448
+	#define RC_FILEVERSION_STRING "1, 3, 2097, 11448\0"
+	static const char FULLVERSION_STRING[] = "1.3.2097.11448";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 383;
+	static const long BUILD_HISTORY = 397;
 	
 
 }
