195aa81f17085246db509cf845c3f03bf505fa27
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-01-22 13:52:33 +0000

    Fixed: infinite loop in OneButtonBandit... Hopefully now it's at least possible to finish the game each time. But the reels may still misbehave...

--- version.h
+++ version.h
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 4;
-	static const long BUILD = 2145;
-	static const long REVISION = 11717;
+	static const long BUILD = 2147;
+	static const long REVISION = 11722;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4625;
-	#define RC_FILEVERSION 1,4,2145,11717
-	#define RC_FILEVERSION_STRING "1, 4, 2145, 11717\0"
-	static const char FULLVERSION_STRING[] = "1.4.2145.11717";
+	static const long BUILDS_COUNT = 4629;
+	#define RC_FILEVERSION 1,4,2147,11722
+	#define RC_FILEVERSION_STRING "1, 4, 2147, 11722\0"
+	static const char FULLVERSION_STRING[] = "1.4.2147.11722";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 45;
+	static const long BUILD_HISTORY = 47;
 	
 
 }
