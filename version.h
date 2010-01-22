77961eac485116439e0cecf098137452f63f96c0
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-01-22 13:38:19 +0000

    Fixed: premature typing freeze bug in SpellingContest

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "01";
+	static const char DATE[] = "22";
 	static const char MONTH[] = "01";
 	static const char YEAR[] = "2010";
 	static const double UBUNTU_VERSION_STYLE = 10.01;
@@ -15,18 +15,18 @@ namespace AutoVersion{
 	
 	//Standard Version Type
 	static const long MAJOR = 1;
-	static const long MINOR = 3;
-	static const long BUILD = 2097;
-	static const long REVISION = 11448;
+	static const long MINOR = 4;
+	static const long BUILD = 2145;
+	static const long REVISION = 11717;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4539;
-	#define RC_FILEVERSION 1,3,2097,11448
-	#define RC_FILEVERSION_STRING "1, 3, 2097, 11448\0"
-	static const char FULLVERSION_STRING[] = "1.3.2097.11448";
+	static const long BUILDS_COUNT = 4625;
+	#define RC_FILEVERSION 1,4,2145,11717
+	#define RC_FILEVERSION_STRING "1, 4, 2145, 11717\0"
+	static const char FULLVERSION_STRING[] = "1.4.2145.11717";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 397;
+	static const long BUILD_HISTORY = 45;
 	
 
 }
