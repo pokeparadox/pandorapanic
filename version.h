478030605c5be6026a3d69d30d7bf82735c41f54
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-02-05 19:39:32 +0000

    Update: Minor tweaking of cbp file

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "22";
-	static const char MONTH[] = "01";
+	static const char DATE[] = "05";
+	static const char MONTH[] = "02";
 	static const char YEAR[] = "2010";
-	static const double UBUNTU_VERSION_STYLE = 10.01;
+	static const double UBUNTU_VERSION_STYLE = 10.02;
 	
 	//Software Status
 	static const char STATUS[] = "Release Candidate";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 4;
-	static const long BUILD = 2147;
-	static const long REVISION = 11722;
+	static const long BUILD = 2205;
+	static const long REVISION = 12057;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4629;
-	#define RC_FILEVERSION 1,4,2147,11722
-	#define RC_FILEVERSION_STRING "1, 4, 2147, 11722\0"
-	static const char FULLVERSION_STRING[] = "1.4.2147.11722";
+	static const long BUILDS_COUNT = 4734;
+	#define RC_FILEVERSION 1,4,2205,12057
+	#define RC_FILEVERSION_STRING "1, 4, 2205, 12057\0"
+	static const char FULLVERSION_STRING[] = "1.4.2205.12057";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 47;
+	static const long BUILD_HISTORY = 105;
 	
 
 }
