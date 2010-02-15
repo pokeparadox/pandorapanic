6010d9a44b9e5346759574f65163936165129435
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-02-15 13:38:58 +0000

    Fixed: Minor blending issues in StateCredits

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "10";
+	static const char DATE[] = "15";
 	static const char MONTH[] = "02";
 	static const char YEAR[] = "2010";
 	static const double UBUNTU_VERSION_STYLE = 10.02;
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 4;
-	static const long BUILD = 2285;
-	static const long REVISION = 12457;
+	static const long BUILD = 2288;
+	static const long REVISION = 12473;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4879;
-	#define RC_FILEVERSION 1,4,2285,12457
-	#define RC_FILEVERSION_STRING "1, 4, 2285, 12457\0"
-	static const char FULLVERSION_STRING[] = "1.4.2285.12457";
+	static const long BUILDS_COUNT = 4885;
+	#define RC_FILEVERSION 1,4,2288,12473
+	#define RC_FILEVERSION_STRING "1, 4, 2288, 12473\0"
+	static const char FULLVERSION_STRING[] = "1.4.2288.12473";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 185;
+	static const long BUILD_HISTORY = 188;
 	
 
 }
