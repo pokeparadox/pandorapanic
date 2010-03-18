c3d5dda6a120fa0eeb935bfad2483d57736a2c41
foxblock@gmail.com <foxblock@gmail.com@638b986c-7c96-11de-8963-3b09bc248160>
2010-03-18 20:46:37 +0000

    Added new minigame: TestYourSight That will be all

---
+++ trunk/StateTestSight.cpp
@@ -0,0 +1,304 @@
+#include "StateTestSight.h"
+StateTestSight::StateTestSight()
+{
+    nullify = false;
+}
+
+
+StateTestSight::~StateTestSight()
+{
+
+}
+
+void StateTestSight::init()
+{
+    // vars
+    for (int I = CUPS_AMOUNT-1; I >= 0; --I)
+    {
+        cups[I][ciCurrentPos] = I;
+        cups[I][ciDestination] = I;
+        cups[I][ciVelocity] = 0;
+        cups[I][ciShowCounter] = 0;
+    }
+    diffLevel = min(variables[2].getInt(),20); // affects speed of shuffling and number of swaps
+    phase = gpInit; // phase of the game
+    correctCup = rand()%5; // the correct cup - duh
+    numShuffles = 0; // nummber of swaps already done
+    selection = 0; // the current selected cup (in answer phase)
+
+    // graphic
+    GFX::setClearColour(WHITE);
+    img_cup.loadSprite("images/TestYourSight/cup.png");
+    hand.loadSprite("images/TestYourSight/hand.png");
+    hand.setPosition(LEFT_CUP_X-25,480-232);
+    coin.loadSprite("images/TestYourSight/coin.png");
+    coin.setPosition(LEFT_CUP_X+11 + correctCup * CUP_OFFSET,CUPS_Y + 80);
+    bg.loadBackground("images/TestYourSight/bg.png");
+    pauseText.loadFont("font/bip.ttf", 32);
+    pauseText.setColour(BLACK);
+    buttonSheet.loadFrames("images/ButtonPrompter/ButtonsSheet.png",10,2);
+}
+
+void StateTestSight::userInput()
+{
+
+    input->update();
+    #ifdef PLATFORM_PC
+        if(input->isQuit())
+            nullifyState();
+    #endif
+
+    if (phase == gpAnswer)
+    {
+        // move arm in answer phase
+        if(input->isLeft())
+        {
+            if (selection > 0)
+                selection--;
+        }
+        if(input->isRight())
+        {
+            if (selection < 4)
+                selection ++;
+        }
+
+        // select a cup
+        if(input->isA())
+        {
+            phase = gpConclusion;
+            showCup(selection);
+        }
+    }
+
+    if(input->isStart())
+    {
+        pauseToggle();
+    }
+    input->resetKeys();
+}
+
+#ifdef PENJIN_SDL
+void StateTestSight::render(SDL_Surface *screen)
+{
+    GFX::clearScreen(screen);
+    bg.render(screen);
+
+    // show coin only when cups are not being shuffled
+    if (not (phase == gpShuffle))
+    {
+        coin.setX(LEFT_CUP_X+11 + cups[correctCup][ciCurrentPos] * CUP_OFFSET);
+        coin.render(screen);
+    }
+
+    // z-order sort (using the y-offset)
+    int indexArray[CUPS_AMOUNT] = {0}; // array holding the ordered indexes (referring to yPos)
+    float yPos[CUPS_AMOUNT] = {0.0}; // array for the y-offsets (used for the z-ordering)
+    for (int I = CUPS_AMOUNT-1; I >= 0; --I)
+    {
+        yPos[I] = getYOffset(I);
+    }
+    NumberUtility::sort(yPos,indexArray,false); // sort descending as smaller yOffset means the cup is placed in the back and has to be rendered first
+
+    // draw cups in sorted order
+    for (int I = CUPS_AMOUNT-1; I >= 0; --I)
+    {
+        img_cup.setPosition(LEFT_CUP_X + CUP_OFFSET * cups[indexArray[I]][ciCurrentPos], CUPS_Y + yPos[indexArray[I]] - SHOW_HEIGHT * ( min(cups[indexArray[I]][ciShowCounter],float(SHOW_FRAMES_MOVE)) - max(float(0),cups[indexArray[I]][ciShowCounter] - SHOW_FRAMES_MOVE - SHOW_FRAMES_HOLD)) / SHOW_FRAMES_MOVE);
+        img_cup.render(screen);
+    }
+
+    // draw hand in answer phase
+    if (phase == gpAnswer)
+    {
+        hand.setX(LEFT_CUP_X-25 + selection * CUP_OFFSET);
+        hand.render(screen);
+    }
+}
+
+void StateTestSight::pauseScreen(SDL_Surface* screen)
+{
+    if(!isSubState())
+        pauseSymbol(screen);
+    buttonsOverlay(screen);
+
+    pauseText.setPosition(50,180);
+    pauseText.print(screen, "Test your sight!");
+    pauseText.setPosition(50,220);
+    pauseText.print(screen, "Follow the path of the cup hiding the coin!");
+    pauseText.setPosition(50,260);
+    pauseText.print(screen, "Press     to select your destiny!");
+    buttonSheet.setCurrentFrame(10);
+    buttonSheet.setPosition(134,220);
+    buttonSheet.render(screen);
+}
+#else
+    void StateTestSight::render()
+    {
+
+    }
+    void StateTestSight::pauseScreen()
+    {
+        pauseSymbol();
+    }
+#endif
+
+void StateTestSight::onPause()
+{
+
+}
+
+void StateTestSight::onResume()
+{
+
+}
+
+void StateTestSight::update()
+{
+    moveCups();
+
+    // show initial correct cup
+    cout << isMoving() << endl;
+    if (phase == gpInit)
+    {
+        showCup(correctCup);
+        phase = gpShow;
+    }
+    else if ((phase == gpShow) && not isMoving())
+        phase = gpShuffle;
+    // shuffle
+    else if ((phase == gpShuffle) && not isMoving())
+    {
+        // amount check
+        if (numShuffles == max(diffLevel,5))
+            phase = gpAnswer;
+        else
+        {
+            // get two randomly chosen, different cups
+            int cup1 = rand()%5;
+            int cup2 = rand()%5;
+            while (cup1 == cup2)
+                cup2 = rand()%5;
+            // do the shuffle
+            swapCups(cup1,cup2);
+
+            numShuffles++;
+        }
+    }
+    // answer pahse
+    else if ((phase == gpAnswer) && not isMoving())
+    {
+        // wait for user input
+    }
+    // conclusion phase
+    else if ((phase == gpConclusion) && not isMoving())
+    {
+        // answer correct - goto next state as cup has already been shown after user input
+        if (selection == cups[correctCup][ciCurrentPos])
+        {
+            variables[0].setInt(1);
+            phase = gpExit;
+        }
+        // answer incorrect - show correct cup, then go to next state
+        else
+        {
+            showCup(cups[correctCup][ciCurrentPos]);
+            variables[0].setInt(0);
+            phase = gpExit;
+        }
+    }
+    // Exit game - goto next state
+    else if ((phase == gpExit) && not isMoving())
+        setNextState(STATE_MAIN);
+}
+
+/// Private implementation
+
+// Swaps two cup's positions and sets velocity
+void StateTestSight::swapCups(CRint index1, CRint index2)
+{
+    if (index1 < CUPS_AMOUNT && index2 < CUPS_AMOUNT && index1 != index2)
+    {
+        cups[index1][ciDestination] = round(cups[index2][ciCurrentPos]);
+        cups[index2][ciDestination] = round(cups[index1][ciCurrentPos]);
+        cups[index1][ciVelocity] = round(cups[index2][ciCurrentPos]) - cups[index1][ciCurrentPos];
+        cups[index2][ciVelocity] = round(cups[index1][ciCurrentPos]) - cups[index2][ciCurrentPos];
+    }
+}
+
+// Show what's under a specific cup
+void StateTestSight::showCup(CRint index)
+{
+    for (int I = CUPS_AMOUNT-1; I >= 0; --I)
+        if (cups[I][ciCurrentPos] == index)
+            cups[I][ciShowCounter] = 1;
+}
+
+// Move a cup over to its new position using velocity in percent
+void StateTestSight::moveCups()
+{
+    for (int I = CUPS_AMOUNT-1; I >= 0; --I)
+    {
+        // Check whether velocity is greater than difference left and use that in case
+        cups[I][ciCurrentPos] += min(abs(cups[I][ciVelocity] / CUP_OFFSET * diffLevel), // Normal movement between two spots
+                                     abs(cups[I][ciCurrentPos]-cups[I][ciDestination])) * // Difference left between two spots
+                                     NumberUtility::sign(cups[I][ciVelocity]); // Direction of movement
+        if (hasFinished(I))
+        {
+            cups[I][ciVelocity] = 0;
+        }
+        if (cups[I][ciShowCounter] > 0)
+        {
+            if (cups[I][ciShowCounter] < SHOW_FRAMES_MOVE * 2 + SHOW_FRAMES_HOLD)
+                cups[I][ciShowCounter]++;
+            else
+                cups[I][ciShowCounter] = 0;
+        }
+    }
+}
+
+// Calculates the y offset of a single cup so that cups moved left are moved in front of the other cups and rightwards moving cups in the back
+float StateTestSight::getYOffset(CRint index)
+{
+    const float x = abs(cups[index][ciCurrentPos]-cups[index][ciDestination]);
+    const float vel = abs(cups[index][ciVelocity]);
+    if (vel != 0.0)
+    {
+        const float y = pow( 2.0/vel * (x - (vel/2.0) ), 2.0) - 1.0;
+        return NumberUtility::sign(cups[index][ciVelocity]) * y * MAX_Y_OFFSET;
+    }
+    else
+        return 0.0;
+}
+
+// Checks whether a cup has reached its destination
+bool StateTestSight::hasFinished(CRint index) const
+{
+    if (index < CUPS_AMOUNT)
+    {
+        if (cups[index][ciVelocity] >= 0)
+            return (cups[index][ciCurrentPos] >= cups[index][ciDestination]);
+        else
+            return (cups[index][ciCurrentPos] <= cups[index][ciDestination]);
+    }
+}
+
+// Returns whether one specific cup is moving
+bool StateTestSight::isMoving(CRint index) const
+{
+    if (index < CUPS_AMOUNT)
+    {
+        return ((cups[index][ciVelocity] != 0) || (cups[index][ciShowCounter] != 0));
+    }
+}
+
+// Returns whether at least one cup is moving
+bool StateTestSight::isMoving() const
+{
+    for (int I = CUPS_AMOUNT-1; I >= 0; --I)
+    {
+        if ((cups[I][ciVelocity] != 0) || (cups[I][ciShowCounter] != 0))
+        {
+            return true;
+        }
+    }
+    return false;
+};
---
+++ trunk/StateTestSight.h
@@ -0,0 +1,90 @@
+#ifndef STATETESTSIGHT_H_INCLUDED
+#define STATETESTSIGHT_H_INCLUDED
+
+#include "mgBaseState.h"
+#include "PenjinStates.h"     // Make sure your state is listed in this include.
+
+#include "Sprite.h"
+#include "Background.h"
+#include "NumberUtility.h"
+#include "Text.h"
+#include "AnimatedSprite.h"
+
+#define CUPS_AMOUNT 5
+#define LEFT_CUP_X 200
+#define CUPS_Y 140
+#define CUP_OFFSET 80
+#define MAX_Y_OFFSET 30
+#define SHOW_FRAMES_MOVE 20
+#define SHOW_FRAMES_HOLD 20
+#define SHOW_HEIGHT 40
+
+/***************************************************
+    Test Your Sight minigame for PandoraPanic!
+    Graphics by Cyrax
+    Code by foxblock
+***************************************************/
+
+class StateTestSight : public mgBaseState
+{
+    public:
+        StateTestSight();
+        virtual ~StateTestSight();
+        virtual void init();
+        #ifdef PENJIN_SDL
+            virtual void render(SDL_Surface *screen);
+            virtual void pauseScreen(SDL_Surface* screen);
+        #else
+            virtual void render();
+            virtual void pauseScreen();
+        #endif
+        virtual void update();
+        virtual void onPause();
+        virtual void onResume();
+
+
+        virtual void userInput();
+    private:
+        void swapCups(CRint index1, CRint index2);
+        void showCup(CRint index);
+        void moveCups();
+        float getYOffset(CRint index);
+        bool hasFinished(CRint index) const;
+        bool isMoving(CRint index) const;
+        bool isMoving() const;
+
+        enum CUP_IDENT
+        {
+            ciCurrentPos=0, // the current position (including any current transition)
+            ciDestination, // destination (=current position if not moving)
+            ciVelocity, // movement speed
+            ciShowCounter // animation counter for showCup
+        };
+
+        float cups[CUPS_AMOUNT][4]; // using CUP_IDENT for the second array
+        int diffLevel;
+        int correctCup;
+        int numShuffles;
+        int selection;
+
+        enum GAME_PHASE
+        {
+            gpInit,
+            gpShow,
+            gpShuffle,
+            gpAnswer,
+            gpConclusion,
+            gpExit
+        };
+
+        int phase;
+
+        Background bg;
+        Sprite img_cup;
+        Sprite hand;
+        Sprite coin;
+        Text pauseText;
+        AnimatedSprite buttonSheet;
+};
+
+#endif // STATETESTSIGHT_H_INCLUDED
