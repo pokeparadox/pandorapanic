809ae1e992d4a995c88c8b2a2163ce104d9b1bbc
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-03-23 13:08:55 +0000

    Fixed: minor Text rendering problem in the StateMain status.

--- version.h
+++ version.h
@@ -4,10 +4,10 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char DATE[] = "15";
-	static const char MONTH[] = "02";
+	static const char DATE[] = "23";
+	static const char MONTH[] = "03";
 	static const char YEAR[] = "2010";
-	static const double UBUNTU_VERSION_STYLE = 10.02;
+	static const double UBUNTU_VERSION_STYLE = 10.03;
 	
 	//Software Status
 	static const char STATUS[] = "Release Candidate";
@@ -16,17 +16,17 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long MAJOR = 1;
 	static const long MINOR = 4;
-	static const long BUILD = 2288;
-	static const long REVISION = 12473;
+	static const long BUILD = 2312;
+	static const long REVISION = 12597;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 4885;
-	#define RC_FILEVERSION 1,4,2288,12473
-	#define RC_FILEVERSION_STRING "1, 4, 2288, 12473\0"
-	static const char FULLVERSION_STRING[] = "1.4.2288.12473";
+	static const long BUILDS_COUNT = 4937;
+	#define RC_FILEVERSION 1,4,2312,12597
+	#define RC_FILEVERSION_STRING "1, 4, 2312, 12597\0"
+	static const char FULLVERSION_STRING[] = "1.4.2312.12597";
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	static const long BUILD_HISTORY = 188;
+	static const long BUILD_HISTORY = 212;
 	
 
 }
