afc0c45bcf6b70d2e1dc5c69c595a2b85dbcf030
pokeparadox <pokeparadox@638b986c-7c96-11de-8963-3b09bc248160>
2010-06-10 21:49:59 +0000

    casing of outputted PND

--- createPND.sh
+++ createPND.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mksquashfs ./built/font ./built/images ./built/music ./built/preview ./built/scripts ./built/sounds ./built/index.html ./bin/Pandora/PandoraPanic_R ./built/PXML.xml PandoraPanic.sfs
-cat ./PandoraPanic.sfs ./built/PXML.xml ./built/images/ppicon.png > PandoraPanic.pnd
+cat ./PandoraPanic.sfs ./built/PXML.xml ./built/images/ppicon.png > pandorapanic.pnd
